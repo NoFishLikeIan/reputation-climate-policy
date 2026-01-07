31aeaa950c72b744049dd906457564bdeebeb5f3
root <root@IanDell.>
2026-01-07 15:58:53 +0100

    ✨ Calibration of marginal cost curve

--- scripts/calibration.jl
+++ scripts/calibration.jl
@@ -4,8 +4,12 @@ using DotEnv, UnPack
 using CSV, JLD2
 using DataFrames, TidierData
 using Printf
+using Plots, LaTeXStrings
 
 include("../src/utils.jl")
+
+Plots.default(linewidth = 2, dpi = 180, label = false)
+plotpath = "figures"; if !ispath(plotpath) mkpath(plotpath) end
 
 ar6filepath = "data/AR6_Scenarios_Database_World_v1.1.csv"; @assert isfile(ar6filepath)
 
@@ -19,20 +23,24 @@ scenarios = @chain ar6data begin
     @filter(!occursin("CEMICS_SSP2-Npi", Scenario))
     @filter(!occursin(r"Delayed Transition", Scenario))
     @filter(Scenario == "EN_NoPolicy" || 
-            occursin(r"PkBudg\d+", Scenario) ||          # Carbon budget scenarios
-            occursin(r"EN_INDCi.*_\d+f?$", Scenario) ||  # Carbon budget scenarios  
-            occursin(r"NGFS2_.*(2°C|Net-Zero)", Scenario) ||  # NGFS climate scenarios
-            occursin(r"CEMICS_SSP\d-1p5C", Scenario) ||  # 1.5°C scenarios
-            occursin(r"CEMICS_SSP\d-2C", Scenario))      # 2°C scenarios
+            occursin(r"PkBudg\d+", Scenario) || # Carbon budget scenarios
+            occursin(r"EN_INDCi.*_\d+f?$", Scenario) ||
+            occursin(r"NGFS2_.*(2°C|Net-Zero)", Scenario) || # NGFS climate scenarios
+            occursin(r"CEMICS_SSP\d-1p5C", Scenario) || # 1.5°C scenarios
+            occursin(r"CEMICS_SSP\d-2C", Scenario)) # 2°C scenarios
     @group_by(Model, Scenario)
 end
 
 begin # Construct scenarios dataframes
     npscenario = "EN_NoPolicy"  # True no-policy baseline from REMIND-MAgPIE
+    variablemap = Dict(
+        :c => "Investment|Energy Supply|Electricity|Renewables (incl. Biomass)",
+        :E => "AR6 climate diagnostics|Infilled|Emissions|Kyoto Gases (AR6-GWP100)",
+        :ϕ => "Capacity Additions|Electricity|Renewables (incl. Biomass)",
+        :p => "Price|Carbon"
+    )
 
     parsefloat(year) = parse(Float64, year)
-
-    investmentkey = "Investment|Energy Supply|Electricity|Non-fossil"
 
     dfs = Dict{String, DataFrame}()
     for (k, scenario) in pairs(scenarios)
@@ -45,53 +53,85 @@ begin # Construct scenarios dataframes
             @filter(Year ≥ 2020.)
         end
 
-        if investmentkey in names(df)
+        if all(variable in names(df) for variable in values(variablemap))
             dfs[k.Scenario] = df
         end
     end
+
     dfnp = dfs[npscenario]
     filter!(((scenario, df), ) -> scenario != npscenario, dfs)
     nmodels = length(dfs)
 end
 
 begin # Fill in data coefficients
-    emissionkey = "AR6 climate diagnostics|Infilled|Emissions|Kyoto Gases (AR6-GWP100)"
-
-    ts = Float64[]
-    investments = Float64[]
-    abatements = Float64[]
+    αs = Float64[]
+    κs = Float64[]
+    νs = Float64[]
     for (k, (scenario, df)) in enumerate(dfs)
-        years = df.Year
-
-        investment = df[:, investmentkey] ./ 1000 # t$ / year
-        E = df[:, emissionkey]
-        
-        dfnpmatch = filter(r -> r.Year in years, dfnp)
-        investmentnp = dfnpmatch[:, investmentkey] ./ 1000
-        
-        if maximum(investment - investmentnp) ≤ 0
-            continue
-        end
-        
-        Eⁿᵖ = dfnpmatch[:, emissionkey]
-
-        excessinvestment = investment - investmentnp
+        # aₜ [.]
+        emissionsfactor = 1e-3 # MtCO₂ -> GtCO₂
+        E = df[:, variablemap[:E]] * emissionsfactor
+        Eⁿᵖ = interpolate(dfnp[:, variablemap[:E]] * emissionsfactor, df.Year, dfnp.Year)
         abated = @. 1 - E / Eⁿᵖ
-        ts = df[:, :Year] .- 2020.
 
-        abidx = 0 .< abated .< 1
+        # Limit to 0 ≤ aₜ ≤ 1
+        abidx = eachindex(abated)[@. (0 < abated < 1)]
         abated = abated[abidx]
-        impulseabatement = @. (abated[2:end] - abated[1:(end - 1)]) / (1 - abated[1:(end - 1)])
 
-        ts = ts[abidx][1:(end - 1)]
-        excessinvestment = excessinvestment[abidx][1:(end - 1)]
+        # c(ϕₜ) [t$ / year]
+        costfactor = 1e-3 # b$ → t$
+        costs = df[:, variablemap[:c]] * costfactor
+        costsⁿᵖ = interpolate(dfnp[:, variablemap[:c]] * costfactor, df.Year, dfnp.Year)
+        excesscosts = costs - costsⁿᵖ
+        
+        # ϕₜ [tW / year]
+        capacityfactor = 1e-3 # gW → tW
+        installedcapacity = df[:, variablemap[:ϕ]] .* capacityfactor
+        installedcapacityⁿᵖ = interpolate(dfnp[:, variablemap[:ϕ]] .* capacityfactor, df.Year, dfnp.Year)
+        excessedinstalledcapacity = installedcapacity - installedcapacityⁿᵖ
 
-        yearlyspace = 0:1:maximum(ts)
-        impulseyearly = [interpolate(impulseabatement, t, ts) for t in yearlyspace]
-        investedyearly = [interpolate(excessinvestment, t, ts) for t in yearlyspace]
+        # SCC [t$ / GtCO₂]
+        sccfactor = 1e-3 # ($/tCO₂) → (t$ / GtCO₂)
+        scc = df[:, variablemap[:p]] * sccfactor
 
-        append!(ts, t[idxs])
-        append!(investments, excessinvestment[idxs])
-        append!(abatements, abated[idxs])
+        # Estimation of α [year / tW]
+        t = abidx[1:(end - 1)]
+        impulseabatement = @. (abated[t + 1] - abated[t]) / (1 - abated[t])
+        yearlytime = range(extrema(df.Year[t])..., step = 5.) # Interpolate data every five years
+
+        Aₜ = interpolate(impulseabatement, yearlytime, df.Year[t])
+        φₜ = interpolate(excessedinstalledcapacity[t], yearlytime, df.Year[t])
+        
+        α = (φₜ'φₜ) \ (φₜ'Aₜ)
+        
+        # Estimation of κ [t$ / tW] and ν [(t$ / tW)²]
+        pₜ = interpolate(scc[t], yearlytime, df.Year[t])
+        Eₜⁿᵖ = interpolate(Eⁿᵖ[t], yearlytime, df.Year[t])
+        aₜ = interpolate(abated[t], yearlytime, df.Year[t])
+
+        λₜ = @. pₜ * Eₜⁿᵖ * α * (1 - aₜ)
+        x = [ones(length(φₜ)) φₜ]
+        κ, ν = (x'x) \ (x'λₜ)
+
+        push!(αs, α)
+        push!(κs, κ)
+        push!(νs, ν)
     end
+
+    @printf "Estimated α ≈ %.2f (%.2f)\n" mean(αs) std(αs)
+    @printf "Estimated κ ≈ %.2f (%.2f)\n" mean(κs) std(κs)
+    @printf "Estimated ν ≈ %.2f (%.2f)\n" mean(νs) std(νs)
 end
+
+let # Plot histograms
+    p1 = histogram(αs, bins=12, xlabel=L"\alpha \; [\mathrm{year/tW}]", legend=false, c = :white)
+    p2 = histogram(κs, bins=12, xlabel=L"\kappa \; [\mathrm{trUSD/tW}]", legend=false, c = :white)
+    p3 = histogram(νs, bins=12, xlabel=L"\nu \; [\mathrm{trUSD/tW}^2]", legend=false, c = :white)
+    
+    estfig = plot(p1, p2, p3, layout=(1,3), size=(1200, 400), margins = 10Plots.mm)
+
+    savefig(estfig, joinpath(plotpath, "calibration", "estimation.png"))
+
+    estfig
+end
+
