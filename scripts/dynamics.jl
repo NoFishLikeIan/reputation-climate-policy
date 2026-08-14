9509d28e63e8d119b1f20dbe02a2fda9d130593a
nofishlikeian <andreatitton96@gmail.com>
2026-08-14 18:41:01 +0200

    Added tax figure

--- scripts/dynamics.jl
+++ scripts/dynamics.jl
@@ -67,59 +67,86 @@ policies = constructpolicies(solution, parameters, grid)
 ## Simulate path
 x₀ = SA.SVector(0.5, climate.m₀, firm.a₀)
 dynamicparameters = (policies, parameters, grid);
+endtime = activeterminal
 
 dynamicfn = SDE.SDEFunction{false}(dynamicdrift, dynamicnoise)
-dynamicprob = SDE.SDEProblem(dynamicfn, x₀, (0, parameters.horizon), dynamicparameters)
+dynamicprob = SDE.SDEProblem(dynamicfn, x₀, (0, endtime), dynamicparameters)
 ensembleproblem = SDE.EnsembleProblem(dynamicprob)
 
 φs = [0.1, 0.2, 0.5, 0.75, 0.9, 1.0]
-
-solutions = SciMLBase.EnsembleSolution[] 
+EnsemblePolicy = Vector{Vector{NTuple{3, Float64}}}
+solutions = SciMLBase.EnsembleSolution[]
+policyensembles = EnsemblePolicy[] 
 for φ₀ in φs
     Printf.@printf "Solving φ₀ = %.1f\r" φ₀
     x₀ = SA.SVector(φ₀, climate.m₀, firm.a₀)
     sol = SDE.solve(ensembleproblem, SDE.SOSRI(); u0 = x₀, trajectories = 10_000)
 
+    policyensemble = Vector{NTuple{3, Float64}}[]
+    for soli in sol.u
+        policytraj = [ policy(t, u, policies, parameters, grid) for (t, u) in zip(soli.t, soli.u) ]
+        push!(policyensemble, policytraj)
+    end
+
     push!(solutions, sol)
+    push!(policyensembles, policyensemble)
 end
 
-##
+## Plot
 figurepath = joinpath("figures", solutionlabel(climate, government, firm, signal))
-
 !ispath(figurepath) && mkpath(figurepath)
-
-begin
+let
     nφ = length(φs)
     beliefcolors = Plots.palette(:Dark2_3, nφ)
     beliefsfigures = Plots.Plot[]
     concentrationfigures = Plots.Plot[]
     abatemnetfigures = Plots.Plot[]
+    taxfigures = Plots.Plot[]
 
     for (i, φ₀) in enumerate(φs)
-        belieffigure = Plots.hline(ylims = (0, 1), xlabel = "Year", title = L"$\phi_0 = %$(φ₀)$")
-        concentrationfig = Plots.hline(ylims = extrema(grid.mgrid), xlabel = "Year", ylabel = "GtCO2", title = L"$\phi_0 = %$(φ₀)$")
-        abatementfigure = Plots.hline(ylims = (0, firm.e₀), xlabel = "Year", ylabel = "GtCO2 per year", title = L"$\phi_0 = %$(φ₀)$")
-
+        Printf.@printf "Plotting φ₀ = %.1f\r" φ₀
         dynamicsol = solutions[i]
-        Plots.plot!(belieffigure, dynamicsol; idxs = 1, alpha = 0.25, c = beliefcolors[i])
-        Plots.plot!(concentrationfig, dynamicsol; idxs = 2, alpha = 0.25, c = beliefcolors[i])
-        Plots.plot!(abatementfigure, dynamicsol; idxs = 3, alpha = 0.25, c = beliefcolors[i])
+        c = beliefcolors[i]
+
+        # State
+        belieffigure = Plots.plot(ylims = (0, 1), xlabel = "Year", title = L"$\phi_0 = %$(φ₀)$")
+        concentrationfig = Plots.plot(ylims = extrema(grid.mgrid), xlabel = "Year", ylabel = "GtCO2", title = L"$\phi_0 = %$(φ₀)$")
+        abatementfigure = Plots.plot(ylims = (0, firm.e₀), xlabel = "Year", ylabel = "GtCO2 per year", title = L"$\phi_0 = %$(φ₀)$")
+
+        Plots.plot!(belieffigure, dynamicsol; idxs = 1, alpha = 0.25, c)
+        Plots.plot!(concentrationfig, dynamicsol; idxs = 2, alpha = 0.25, c)
+        Plots.plot!(abatementfigure, dynamicsol; idxs = 3, alpha = 0.25, c)
 
         push!(beliefsfigures, belieffigure)
         push!(concentrationfigures, concentrationfig)
         push!(abatemnetfigures, abatementfigure)
+
+        # Policy
+        policyensemble = policyensembles[i]
+        timetraj = dynamicsol.u[1].t
+        τᶜtraj = getindex.(policyensemble[1], 2) ./ taxfactor
+        taxfigure = Plots.plot(timetraj, τᶜtraj; c, linestyle = :dash, xlabel = "Year", ylabel = "USD per tCO2")
+
+        for (i, policytraj) in enumerate(policyensemble)
+            τtraj = getindex.(policytraj, 1)
+            Plots.plot!(dynamicsol.u[i].t, τtraj ./ taxfactor; alpha = 0.25, c)
+        end
+
+        push!(taxfigures, taxfigure)
     end
 
     rows = isqrt(nφ)
     columns = nφ - rows
 
-    joinbeliefsfigure = Plots.plot(beliefsfigures...; layout = (rows, columns), size = 1000 .* (√2, 1), plot_title = L"Belief $\phi$", ylims = (0, 1))
-    jointconcentrationfig = Plots.plot(concentrationfigures...; layout = (rows, columns), size = 1000 .* (√2, 1), plot_title = L"Concentration $m$", ylims = extrema(grid.mgrid))
-    jointabatementfigure = Plots.plot(abatemnetfigures...; layout = (rows, columns), size = 1000 .* (√2, 1), plot_title = L"Abatemnet $a$", ylims = (0, firm.e₀))
+    beliefsfigjoint = Plots.plot(beliefsfigures...; layout = (rows, columns), size = 1000 .* (√2, 1), plot_title = L"Belief $\phi$", ylims = (0, 1))
+    concentrationfigjoint = Plots.plot(concentrationfigures...; layout = (rows, columns), size = 1000 .* (√2, 1), plot_title = L"Concentration $m$", ylims = extrema(grid.mgrid))
+    abatemnetfigjoint = Plots.plot(abatemnetfigures...; layout = (rows, columns), size = 1000 .* (√2, 1), plot_title = L"Abatemnet $a$", ylims = (0, firm.e₀))
+    taxfigjoint = Plots.plot(taxfigures...; layout = (rows, columns), size = 1000 .* (√2, 1), plot_title = L"Tax $\tau$")
 
-    Plots.savefig(joinbeliefsfigure, joinpath(figurepath, "beliefs.png"))
-    Plots.savefig(jointconcentrationfig, joinpath(figurepath, "concentration.png"))
-    Plots.savefig(jointabatementfigure, joinpath(figurepath, "abatement.png"))
+    Plots.savefig(beliefsfigjoint, joinpath(figurepath, "beliefs.png"))
+    Plots.savefig(concentrationfigjoint, joinpath(figurepath, "concentration.png"))
+    Plots.savefig(abatemnetfigjoint, joinpath(figurepath, "abatement.png"))
+    Plots.savefig(taxfigjoint, joinpath(figurepath, "tax.png"))
 
     println("Saved figures in ", figurepath)
 end
